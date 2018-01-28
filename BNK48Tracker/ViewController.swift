98d77ffa882cab112caaba43f322e52164572146
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2018-01-28 15:00:22 +0700

    Add select image from photo library

--- BNK48Tracker/ViewController.swift
+++ BNK48Tracker/ViewController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     // MARK: - Properties
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     // MARK: - ViewController func
     override func viewDidLoad() {
         super.viewDidLoad()
         self.nameTextField.delegate = self
+        self.photoImageView.isUserInteractionEnabled = true
     }
     
     // MARK: - UITextFieldDelegate
@@ -31,11 +33,30 @@ class ViewController: UIViewController,UITextFieldDelegate {
         self.nameLabel.text = textField.text
     }
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated:  true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        self.photoImageView.image = selectedImage
+          dismiss(animated:  true, completion: nil)
+    }
+    
     // MARK: - Actions
     @IBAction func setLabel(_ sender: Any) {
         self.nameLabel.text = self.nameTextField.text
     }
+    
     @IBAction func setImageFromLibrary(_ sender: Any) {
+        self.nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        self.present(imagePickerController,animated: true, completion: nil)
     }
 }
 
