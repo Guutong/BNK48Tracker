5f25d4f812ad96de3824801cfc65e3d78446cea5
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2018-01-28 09:48:59 +0700

    Initial Commit

---
+++ BNK48Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BNK48Tracker
+//
+//  Created by Pornmongkon Pongsai on 28/1/2561 BE.
+//  Copyright © 2561 Pornmongkon Pongsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
